5411b1445036b389cd8055bd49e190d159579840
Ignacio javier Sayagués Báez <IS308887@fi365.ort.edu.uy>
2026-06-08 21:13:10 -0300

    feat: add EstadoResultados API endpoints

--- ProyectoIntegrador.Service/Interfaces/IEstadoResultadoService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ProyectoIntegrador.Service.DTOs;
-
-namespace ProyectoIntegrador.Service.Interfaces
-{
-    public interface IEstadoResultadoService
-    {
-        Task<EstadoResultadosResponseDto> GenerarEstadoResultados(Guid clienteId, DateOnly fechaDesde, DateOnly fechaHasta);
-    }
-}
