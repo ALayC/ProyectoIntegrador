33de787d222d17eb2b307d72bbc2cc92ae04bd2e
Ignacio Sayagués <IS308887@fi365.ort.edu.uy>
2026-06-29 20:46:30 -0300

    Implement balance general backend (#39)
    * feat: add balancegeneraldto
    
    * feat: add balancegeneralservice
    
    * feat: implement obtenerparabalancegeneral in lineaasientorepository
    
    * feat: implement balancegenralservice
    
    * refactor: balancegenralservice
    
    * feat: implement api-balancegeneralcontroller

---
+++ ProyectoIntegrador.Service/Interfaces/IBalanceGeneralService.cs
@@ -0,0 +1,9 @@
+﻿using ProyectoIntegrador.Service.DTOs;
+
+namespace ProyectoIntegrador.Service.Interfaces
+{
+    public interface IBalanceGeneralService
+    {
+        Task<BalanceGeneralResponseDto> Generar(BalanceGeneralFiltroDto filtro);
+    }
+}
