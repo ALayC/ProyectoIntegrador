4d0698544ff9e564e9e29ec1e2e64a1ee3bcdceb
Ignacio javier Sayagués Báez <IS308887@fi365.ort.edu.uy>
2026-06-30 18:58:04 -0300

    delete appsettings.production

---
+++ ProyectoIntegrador.UI/Controllers/BalanceGeneralController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoIntegrador.UI.Controllers
+{
+    public class BalanceGeneralController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
